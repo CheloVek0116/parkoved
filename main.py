2b61e09735139c64d20ee033f38d87e81c7fdea0
AndrewBalakirev <megabalabol@mail.ru>
2020-09-25 19:12:21 +0700

    init

---
+++ main.py
@@ -0,0 +1,16 @@
+import telebot
+
+
+bot = telebot.TeleBot('1168033372:AAGzGQpmB9sYLJHPp_v1gtgjPk_DrM5ATew')
+
+
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    if message.text == "Привет":
+        bot.send_message(message.from_user.id, "Привет, чем я могу тебе помочь?")
+    elif message.text == "/help":
+        bot.send_message(message.from_user.id, "Напиши привет")
+    else:
+        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши /help.")
+
+bot.polling(none_stop=True, interval=0)
